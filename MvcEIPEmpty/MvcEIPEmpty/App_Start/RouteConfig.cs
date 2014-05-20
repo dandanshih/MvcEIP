33e780939b427e4d7ad66d4e52146929bc259aba
dandanshih <s1321519@gmail.com>
2014-05-20 17:26:13 +0800

    [新增] MVC 的空專案, MVC 加入了 Index.apsx 和首頁

---
+++ MvcEIPEmpty/MvcEIPEmpty/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcEIPEmpty
+{
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			// 設定 Default 是在 Home / Index 目錄下
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", 
+				id = UrlParameter.Optional }
+			);
+		}
+	}
+}
---
+++ MvcEIPEmpty/MvcEIPEmpty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcEIPEmpty.MvcApplication" Language="C#" %>
